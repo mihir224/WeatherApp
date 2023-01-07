59c612ceab7ea8d91f807488816a7d60c8f2f0b8
mihir224 <mihirsaini25@gmail.com>
2023-01-08 00:58:36 +0530

    hide api key

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const API_KEY=require(__dirname+"/hidden.js");
 const express=require("express");
 const https=require("https");
 const bodyParser=require("body-parser");
@@ -7,9 +8,8 @@ app.get('/',function(req,res){
     res.sendFile(__dirname+"/index.html");
     app.post("/",function(req,res){
         const query=req.body.city;
-        const API_KEY="<INSERT-APIKEY>";
         const unit="metric";
-        https.get("https://api.openweathermap.org/data/2.5/weather?q="+query+"&appid="+API_KEY+"&units="+unit,function(response){
+        https.get("https://api.openweathermap.org/data/2.5/weather?q="+query+"&appid="+API_KEY.API+"&units="+unit,function(response){
         response.on("data", function(data){
             const weatherData=JSON.parse(data);
             console.log(weatherData);
@@ -20,8 +20,8 @@ app.get('/',function(req,res){
             const icon=weatherData.weather[0].icon;
             const imageUrl="http://openweathermap.org/img/wn/" + icon + "@2x.png";
             //to send multiple html items at once
-            res.write("<h1>Weather<h1>")
-            res.write("<h2>The Weather in " + query +  " is " + temp + " Degree Celcius</h2>");
+            res.write("<h1>The Weather Channel<h1>")
+            res.write("<h2>It is currently " + temp + " Degree Celcius in " + query + "</h2>");
             res.write("<img src=" +imageUrl+">");
             res.send();
         })
