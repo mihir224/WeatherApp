417ba3e4e5ba65166c503cca62ddadf1dada7cc9
mihir224 <mihirsaini25@gmail.com>
2023-01-09 23:52:11 +0530

    added css

--- app.js
+++ app.js
@@ -3,11 +3,12 @@ const express=require("express");
 const https=require("https");
 const bodyParser=require("body-parser");
 const app=express();
-app.use(bodyParser.urlencoded({extended:true}))
+app.use(express.static("public")); //to use static files like css
+app.use(bodyParser.urlencoded({extended:true}));
 app.get('/',function(req,res){
     res.sendFile(__dirname+"/index.html");
     app.post("/",function(req,res){
-        const query=req.body.city;
+        const query=req.body.city; //to read the city entered by use
         const unit="metric";
         https.get("https://api.openweathermap.org/data/2.5/weather?q="+query+"&appid="+API_KEY.API+"&units="+unit,function(response){
         response.on("data", function(data){
@@ -20,9 +21,9 @@ app.get('/',function(req,res){
             const icon=weatherData.weather[0].icon;
             const imageUrl="http://openweathermap.org/img/wn/" + icon + "@2x.png";
             //to send multiple html items at once
-            res.write("<h1>The Weather Channel<h1>")
+            res.write("<h1>The Weather Channel</h1>")
             res.write("<h2>It is currently " + temp + " Degree Celcius in " + query + "</h2>");
-            res.write("<img src=" +imageUrl+">");
+            res.write("<img src=" +imageUrl+"></img>");
             res.send();
         })
     })
